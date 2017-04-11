aaa41dd755b2aa01caede3611b93abe07ab21e74
Shashi Shaw <shashi.shaw@silicus.com>
2017-04-11 11:49:33 +0530

    SS - Added initial empty Xcode project.

---
+++ SampleRTC/SampleRTC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleRTC
+//
+//  Created by Shashi Shaw on 11/04/17.
+//  Copyright © 2017 Silicus India Pvt Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleRTC/SampleRTC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleRTC
+//
+//  Created by Shashi Shaw on 11/04/17.
+//  Copyright © 2017 Silicus India Pvt Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
